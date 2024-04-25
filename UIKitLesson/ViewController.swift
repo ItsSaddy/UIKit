5f8338103e51d6aa702d04deb09af063922f4665
Saddam <leetsaddam@mail.ru>
2024-04-25 19:43:07 +0500

    feat(Slider): first meet and work with slider

--- UIKitLesson/ViewController.swift
+++ UIKitLesson/ViewController.swift
@@ -8,24 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+    //MARK: - Properties
     @IBOutlet weak var segmentedControl: UISegmentedControl!
-    
+    @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var label: UILabel!
     
-    
+    //MARK: - viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-        label.font = label.font.withSize(35)
-        label.textAlignment = .center
-        label.numberOfLines = 2
-        label.text = "The first segment is selcted"
-        label.textColor = .red
-        
-        segmentedControl.insertSegment(withTitle: "Third", at: 2, animated: true)
-        
+        setupViews()
     }
     
+    //MARK: - IBActions
     @IBAction func choiceSegment(_ sender: UISegmentedControl) {
         
         label.isHidden = false
@@ -45,6 +39,30 @@ class ViewController: UIViewController {
         }
     }
     
+    @IBAction func sliderAction(_ sender: UISlider) {
+        label.text = sender.value.description
+        
+        let backgroundColor = self.view.backgroundColor
+        self.view.backgroundColor = backgroundColor?.withAlphaComponent(CGFloat(sender.value))
+    }
     
     
+}
+
+//MARK: - Setup views
+extension ViewController {
+    func setupViews() {
+        label.text = slider.value.description
+        label.font = label.font.withSize(35)
+        label.textAlignment = .center
+        label.numberOfLines = 2
+        
+        segmentedControl.insertSegment(withTitle: "Third", at: 2, animated: true)
+        
+        slider.value = 1
+        slider.minimumValue = 0
+        slider.maximumValue = 1
+        slider.minimumTrackTintColor = .darkGray
+        slider.maximumTrackTintColor = .lightGray
+    }
 }
