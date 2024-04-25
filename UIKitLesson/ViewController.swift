124b4cbebe6168c2013ffacc8f9218f2c5f1deaf
Saddam <leetsaddam@mail.ru>
2024-04-25 17:39:36 +0500

    feat(button and label): worked with button and label

--- UIKitLesson/ViewController.swift
+++ UIKitLesson/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet var actionButtons: [UIButton]!
+    @IBOutlet weak var label: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        label.isHidden = true
+        label.font = label.font.withSize(35)
+        label.textColor = .red
+        
+        button.isHidden = true
+        for button in actionButtons{
+            button.setTitleColor(.blue, for: .normal)
+            button.backgroundColor = .green
+        }
+        
+        button.setTitle("Clear", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .red
     }
+    
+    @IBAction func pressedButton(_ sender: UIButton) {
+        label.isHidden = false
+        button.isHidden = false
+        
+        if sender.tag == 0 {
+            label.text = "Hello Guys!"
+            label.textColor = .red
+        }
+        else if sender.tag == 1 {
+            label.text = "Hi there!"
+            label.textColor = .blue
+        }
+        else if sender.tag == 2 {
+            label.isHidden = true
+            button.isHidden = true
+        }
 
-
+    }
+    
 }
 
