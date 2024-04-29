1b9446f010e67059b2930882519dde495be27b7b
Saddam <leetsaddam@mail.ru>
2024-04-29 16:03:50 +0500

    feat(UiTextView): first meet and work with UITextView feat(UiStepper): first meet and work with UIStepper

--- UIKitLesson/ViewController.swift
+++ UIKitLesson/ViewController.swift
@@ -6,27 +6,14 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
-    //MARK: - IBOutlets
-    @IBOutlet weak var segmentedControl: UISegmentedControl!
-    @IBOutlet weak var textFiled: UITextField!
-    @IBOutlet weak var slider: UISlider!
-    @IBOutlet weak var datePicker: UIDatePicker!
-    @IBOutlet weak var toggle: UISwitch!
-    @IBOutlet weak var switchLabel: UILabel!
-    @IBOutlet weak var doneButton: UIButton!
-    @IBOutlet weak var label: UILabel!
-    
-    //MARK: - Properties
-    var uiElements = ["UISegmentedControl",
-                      "UILabel",
-                      "UISlider",
-                      "UITextField",
-                      "UIButton",
-                      "UIDatePicker"]
-    
-    var selectedElement: String?
+    //MARK: - @IBOutlets
+    @IBOutlet weak var stepper: UIStepper!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var textViewButtonConstraint: NSLayoutConstraint!
     
     //MARK: - viewDidLoad
     override func viewDidLoad() {
@@ -34,196 +21,78 @@ class ViewController: UIViewController {
         setupViews()
     }
     
-    //MARK: - IBActions
-    @IBAction func choiceSegment(_ sender: UISegmentedControl) {
+    //MARK: - @IBActions
+    @IBAction func fontSize(_ sender: UIStepper) {
+        let font = textView.font?.fontName
+        let fontSize = CGFloat(sender.value)
         
-        label.isHidden = false
-        
-        switch segmentedControl.selectedSegmentIndex {
-        case 0:
-            label.text = "Now the color has turned red"
-            label.textColor = .red
-        case 1:
-            label.text = "Now the color has turned blue"
-            label.textColor = .blue
-        case 2:
-            label.text = "Now the color has turned green"
-            label.textColor = .green
-        default:
-            print("smthing went wrong")
-        }
+        textView.font = UIFont(name: font!, size: fontSize)
+    }
+}
+
+//MARK: - UITextViewDelegate
+extension ViewController: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textView.backgroundColor = .gray
+        textView.textColor = .lightGray
     }
     
-    @IBAction func sliderAction(_ sender: UISlider) {
-        label.text = sender.value.description
-        
-        let backgroundColor = self.view.backgroundColor
-        self.view.backgroundColor = backgroundColor?.withAlphaComponent(CGFloat(sender.value))
+    func textViewDidEndEditing(_ textView: UITextView) {
+        textView.backgroundColor = self.view.backgroundColor
+        textView.textColor = .black
     }
     
-    @IBAction func donePressed(_ sender: UIButton) {
-        guard let text = textFiled.text, !text.isEmpty else { return }
-        let isOnlyLetter = text.reduce(true) { $0 && $1.isLetter }
+    func textView(_ textView: UITextView,
+                  shouldChangeTextIn range: NSRange,
+                  replacementText text: String) -> Bool {
+        countLabel.text = (textView.text.count + (text.count - range.length)).description
+        return textView.text.count + (text.count - range.length) < 60
+    }
+    
+    @objc func updateTextView(notification: Notification) {
+        guard
+            let userInfo = notification.userInfo as? [String: Any],
+            let keyboardFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
+            else { return }
         
-        label.isHidden = false
-        
-        if isOnlyLetter {
-            label.text = text
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            textView.contentInset = UIEdgeInsets.zero
         }
         else {
-            let alert = UIAlertController(title: "Wrong format", message: "Please, enter your name", preferredStyle: .alert)
-            let okAlert = UIAlertAction(title: "OK", style: .default, handler: nil)
-            alert.addAction(okAlert)
-            present(alert, animated: true)
+            textView.contentInset = UIEdgeInsets(top: 0,
+                                                 left: 0,
+                                                 bottom: keyboardFrame.height - textViewButtonConstraint.constant,
+                                                 right: 0)
+            textView.scrollIndicatorInsets = textView.contentInset
         }
         
-        textFiled.text = nil
-        
-    }
-    
-    @IBAction func chaneDate(_ sender: UIDatePicker) {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateStyle = .full
-        
-        dateFormatter.locale = Locale(identifier: "tg")
-        let dateValue = dateFormatter.string(from: sender.date)
-        label.text = dateValue
-    }
-    
-    @IBAction func toggleSwitch(_ sender: UISwitch) {
-        segmentedControl.isHidden.toggle()
-        label.isHidden.toggle()
-        slider.isHidden.toggle()
-        textFiled.isHidden.toggle()
-        datePicker.isHidden.toggle()
-        doneButton.isHidden.toggle()
-        
-        if !sender.isOn {
-            switchLabel.text = "Скрыть все элементы"
-        }
-        else {
-            switchLabel.text = "Отобразить все элементы"
-        }
+        textView.scrollRangeToVisible(textView.selectedRange)
     }
     
 }
 
-//MARK: - UIPickerView: Delegate, DataSource
-extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
-    
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        uiElements.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        uiElements[row]
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        selectedElement = uiElements[row]
-        textFiled.text = selectedElement
-        
-        hideAllElements()
-        
-        switch selectedElement {
-        case "UISegmentedControl":
-            segmentedControl.isHidden = false
-        case "UILabel":
-            label.isHidden = false
-        case "UISlider":
-            slider.isHidden = false
-        case "UIDatePicker":
-            datePicker.isHidden = false
-        case "UIButton":
-            doneButton.isHidden = false
-        default:
-            break
-        }
-    }
-    
-    private func pickerView(_ pickerView: UIPickerView, viewForRow row: Int , widthForComponent component: Int, reusing view: UIView) -> UIView {
-        
-        var pickerViewLabel = UILabel()
-        
-        if let currentLabel = view as? UILabel {
-            pickerViewLabel = currentLabel
-        }
-        else {
-            pickerViewLabel = UILabel()
-        }
-        pickerViewLabel.textColor = .white
-        pickerViewLabel.textAlignment = .center
-        pickerViewLabel.font = UIFont(name: "AppleSDGothicNeo-Regular", size: 23)
-        pickerViewLabel.text = uiElements[row]
-        
-        return pickerViewLabel
-    }
-    
-    
-}
-
-
-
-
-//MARK: - Methods
+//MARK: - Touch handling
 extension ViewController {
-    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        self.view.endEditing(true)
+    }
+}
+
+//MARK: Setup Views
+private extension ViewController {
     func setupViews() {
+        textView.delegate = self
         
-        label.text = slider.value.description
-        label.font = label.font.withSize(35)
-        label.textAlignment = .center
-        label.numberOfLines = 2
+        textView.font =  UIFont(name: "AppleSDGothicNeo-Regular", size: 17)
+        textView.layer.cornerRadius = 10
         
-        segmentedControl.insertSegment(withTitle: "Green", at: 2, animated: true)
+        stepper.value = 17
+        stepper.minimumValue = 10
+        stepper.maximumValue = 25
         
-        slider.value = 1
-        slider.minimumValue = 0
-        slider.maximumValue = 1
-        slider.minimumTrackTintColor = .darkGray
-        slider.maximumTrackTintColor = .lightGray
+        NotificationCenter.default.addObserver(self, selector: #selector(updateTextView(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         
-        choiceUiElement()
-        createToolBar()
+        NotificationCenter.default.addObserver(self, selector: #selector(updateTextView(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
-    
-    func choiceUiElement() {
-        let elementPicker = UIPickerView()
-        elementPicker.delegate = self
-        
-        textFiled.inputView = elementPicker
-    }
-    
-    func hideAllElements() {
-        segmentedControl.isHidden = true
-        label.isHidden = true
-        slider.isHidden = true
-        datePicker.isHidden = true
-        doneButton.isHidden = true
-    }
-    
-    func createToolBar() {
-        let toolBar = UIToolbar()
-        toolBar.sizeToFit()
-        
-        let doneButton = UIBarButtonItem(title: "Done",
-                                         style: .plain,
-                                         target: self,
-                                         action: #selector(dismissKeyboard))
-        toolBar.setItems([doneButton], animated: true)
-        toolBar.isUserInteractionEnabled = true
-        
-        textFiled.inputAccessoryView = toolBar
-    }
-        
-    
-    
-    @objc func dismissKeyboard() {
-        view.endEditing(true)
-    }
-    
 }
